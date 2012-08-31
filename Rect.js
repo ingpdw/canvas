71758e97657cd5f482acb076e75f84ce0d0b0ba1
parkdongwook <ingpdw@gmail.com>
2012-08-31 16:51:09 +0900

    build

--- Rect.js
+++ Rect.js
@@ -1,18 +1,14 @@
-nc.Circle = function(opts){
+nc.Rect = function(opts){
 	this._panel;
 	this.opts = {
+		type: 'fill',
 		fillStyle: '#398ea5',
 		lineWidth: 2,
 		strokeStyle: '#99cc00',
-		alpha: 1.0,		
-		cx: 0, 
-		cy: 0,
-		r: 100,
-		s: 0, 
-		e: 0,
-		w: 0,
-		h: 0,
-		x: 0,
+		alpha: 1.0,
+		w: 100,
+		h: 10, 
+		x: 0, 
 		y: 0,
 		nCanvas: true,
 		ctx: null,
@@ -22,26 +18,19 @@ nc.Circle = function(opts){
 		mouseout: null
 	};nc.extend(this.opts, opts);
 	
-	this.opts.w = this.opts.h = this.opts.r * 2 + this.opts.lineWidth * 2;
-	this.opts.x = this.opts.cx - this.opts.r - this.opts.lineWidth;
-	this.opts.y = this.opts.cy - this.opts.r - this.opts.lineWidth;;
-	this.opts.cx = this.opts.cy = this.opts.r + this.opts.lineWidth;;
-	
 	this._init();
-	
-	this._addEvent({click: this.opts.handler && this.opts.handler.click});
-	this._addEvent({mouseover: this.opts.handler && this.opts.handler.mouseover});
-	this._addEvent({mouseout: this.opts.handler && this.opts.handler.mouseout});
+	this._addEvent({click: this.opts.click});
+	this._addEvent({mouseover: this.opts.mouseover});
+	this._addEvent({mouseout: this.opts.mouseout});
 };
-nc.Circle.prototype = new nc.Shape;
-nc.Circle.prototype._init = function(){
+nc.Rect.prototype = new nc.Shape;
+nc.Rect.prototype._init = function(){
 	this._draw();
-	
 	if(this.opts.parent && this.opts.nCanvas) 
 		this.appendTo(this.opts.parent);
 };
-nc.Circle.prototype._draw = function(){
-	var opts = this.opts, ctx, s, e;
+nc.Rect.prototype._draw = function(){
+	var opts = this.opts, ctx;
 	if(opts.nCanvas){
 		this._panel = document.createElement('canvas');
 		if(this.opts.nCanvas) this._panel.style.position = 'absolute';
@@ -54,16 +43,12 @@ nc.Circle.prototype._draw = function(){
 		
 		ctx = this._panel.getContext('2d');
 		this._setStyle(ctx, opts);
+		(opts.type === 'fill')?
+				ctx.fillRect(0, 0, opts.w, opts.h):
+				ctx.strokeRect(0, 0, opts.w, opts.h);
 		
-		s = opts.s * Math.PI / 180;
-		e = opts.e * Math.PI / 180;
-		ctx.beginPath();
-		if(!(opts.s == 0 && opts.e == 360)) ctx.moveTo(opts.cx, opts.cy);
-		ctx.arc(opts.cx, opts.cy, opts.r, s, e, false);
-		ctx.closePath();
-		ctx.fill();
-		if(opts.lineWidth) ctx.stroke();
 	}else{
-		
+		opts.ctx.fillStyle = opts.color;
+		opts.ctx.fillRect(opts.x, opts.y, opts.w, opts.h);
 	}
 };
